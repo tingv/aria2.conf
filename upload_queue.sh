747e4e4f0ef6eb9c3262522159264ca62394b592
TingV <47696436+tingv@users.noreply.github.com>
2021-04-08 06:01:55 +0800

    Create upload_queue.sh

---
+++ upload_queue.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2021 TingV
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/tingv/aria2.conf
+# File name：upload_queue.sh
+# Description: Processing tasks in the upload queue.
+# Version: 1.0
+#
